2551f42976a3ba99ec84c2b3156161b46dc850ea
DavidicGeneration01 <dalade@byupathway.edu >
2026-01-21 22:10:27 +0100

    remove sub folder (nextjs-dashboard)

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  reactStrictMode: true,       // example option
+  swcMinify: true,             // example option for faster builds
+  // Add any other Next.js config options you had in your TS file
+};
+
+module.exports = nextConfig;
